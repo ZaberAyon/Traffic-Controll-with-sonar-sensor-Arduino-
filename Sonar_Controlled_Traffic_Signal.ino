d81da6286eeb3fcf332a3e4da5293e9787d7c32f
ZaberAyon <40652011+ZaberAyon@users.noreply.github.com>
2019-02-11 13:09:07 +0600

    Update Sonar_Controlled_Traffic_Signal.ino

--- Sonar_Controlled_Traffic_Signal.ino
+++ Sonar_Controlled_Traffic_Signal.ino
@@ -1,3 +1,5 @@
+#include <ArduinoJson.h>
+
 const int pingPin1 = 2; // Trigger Pin of Ultrasonic Sensor
 const int echoPin1 = 3;
 // Echo Pin of Ultrasonic Sensor
@@ -17,17 +19,17 @@ const int pingPin6 = 12; // Trigger Pin of Ultrasonic Sensor
 const int echoPin6 = 13;
 
 
-const int led1 = 32; // green1
+const int led1 = 31; // green1
 const int led2 = 33; //yellow1
-const int led3 = 34; //red1
+const int led3 = 35; //red1
 
-const int led4 = 35; //green2
-const int led5 = 36; //yellow2
-const int led6 = 37; //red2
+const int led4 = 37; //green2
+const int led5 = 39; //yellow2
+const int led6 = 41; //red2
 
-const int led7 = 38; //green3
-const int led8 = 39; //yellow3
-const int led9 = 40; //red3
+const int led7 = 43; //green3
+const int led8 = 45; //yellow3
+const int led9 = 47; //red3
 
 int stat = 0;
 int count1=0;
@@ -44,6 +46,7 @@ long microsecondsToCentimeters(long microseconds) {
 
 void setup() {
    Serial.begin(9600); // Starting Serial Terminal
+   Serial1.begin(115200);
 
    pinMode(pingPin1, OUTPUT);
    pinMode(pingPin2, OUTPUT);
@@ -73,13 +76,18 @@ void setup() {
    
 }
 
+StaticJsonBuffer<1000> jsonBuffer;
+JsonObject& root = jsonBuffer.createObject();
+
 void loop() {
-   long duration1, inches1, cm1;
-   long duration2, inches2, cm2;
-   long duration3, inches3, cm3;
-   long duration4, inches4, cm4;
-   long duration5, inches5, cm5;
-   long duration6, inches6, cm6;
+   long duration1, inche1, cm1;
+   long duration2, inche2, cm2;
+   long duration3, inche3, cm3;
+   long duration4, inche4, cm4;
+   long duration5, inche5, cm5;
+   long duration6, inche6, cm6;
+
+   float inches1,inches2,inches3,inches4,inches5,inches6;
 
 
    digitalWrite(led1,LOW);
@@ -98,7 +106,8 @@ void loop() {
    delayMicroseconds(10);
    digitalWrite(pingPin1, LOW);
    duration1 = pulseIn(echoPin1, HIGH);
-   inches1 = microsecondsToInches(duration1);
+   inche1 = microsecondsToInches(duration1);
+   inches1 = (float)inche1;
    
    digitalWrite(pingPin2, LOW);
    delayMicroseconds(2);
@@ -106,7 +115,8 @@ void loop() {
    delayMicroseconds(10);
    digitalWrite(pingPin2, LOW);
    duration2 = pulseIn(echoPin2, HIGH);
-   inches2 = microsecondsToInches(duration2);
+   inche2 = microsecondsToInches(duration2);
+   inches2 = (float)inche2;
    
    digitalWrite(pingPin3, LOW);
    delayMicroseconds(2);
@@ -114,7 +124,8 @@ void loop() {
    delayMicroseconds(10);
    digitalWrite(pingPin3, LOW);
    duration3 = pulseIn(echoPin3, HIGH);
-   inches3 = microsecondsToInches(duration3);
+   inche3 = microsecondsToInches(duration3);
+   inches3 = (float)inche3;
    
    digitalWrite(pingPin4, LOW);
    delayMicroseconds(2);
@@ -122,7 +133,8 @@ void loop() {
    delayMicroseconds(10);
    digitalWrite(pingPin4, LOW);
    duration4 = pulseIn(echoPin4, HIGH);
-   inches4 = microsecondsToInches(duration4);
+   inche4 = microsecondsToInches(duration4);
+   inches4 = (float)inche4;
    
    digitalWrite(pingPin5, LOW);
    delayMicroseconds(2);
@@ -130,7 +142,8 @@ void loop() {
    delayMicroseconds(10);
    digitalWrite(pingPin5, LOW);
    duration5 = pulseIn(echoPin5, HIGH);
-   inches5 = microsecondsToInches(duration5);
+   inche5 = microsecondsToInches(duration5);
+   inches5 = (float)inche5;
    
    digitalWrite(pingPin6, LOW);
    delayMicroseconds(2);
@@ -138,11 +151,9 @@ void loop() {
    delayMicroseconds(10);
    digitalWrite(pingPin6, LOW);
    duration6 = pulseIn(echoPin6, HIGH);
-   inches6 = microsecondsToInches(duration6);
+   inche6 = microsecondsToInches(duration6);
+   inches6 = (float)inche6;
   
-   
-   
-   
    
    
    //cm1 = microsecondsToCentimeters(duration1);
@@ -165,99 +176,484 @@ void loop() {
    
    Serial.println();
 
-  if((inches1>0 && inches1<10)&&(inches2>0 && inches2<10))
+    if(isnan(inches1)||isnan(inches2)||isnan(inches3)||isnan(inches4)||isnan(inches5)||isnan(inches6))
   {
-    stat =1;
-    digitalWrite(led1,HIGH);
-    digitalWrite(led2,LOW);
-    digitalWrite(led3,LOW);
-    digitalWrite(led4,LOW);
-    digitalWrite(led5,HIGH);
-
-    digitalWrite(led6,HIGH);
-    //digitalWrite(led6,LOW);
-
-    digitalWrite(led7,LOW);
-    digitalWrite(led8,HIGH);
-  
-    digitalWrite(led9,HIGH);
-    //digitalWrite(led9,LOW);
-    
-    
+    return;
   }
 
-  else if((inches3>0 && inches3<10)&&(inches4>0 && inches4<10))
+  root["in1"] = inches1;
+  root["in2"] = inches2;
+  root["in3"] = inches3;
+  root["in4"] = inches4;
+  root["in5"] = inches5;
+  root["in6"] = inches6;
+
+  if(Serial1.available()>0)
   {
-    stat =1;
-    digitalWrite(led4,HIGH);
-    digitalWrite(led5,LOW);
-    digitalWrite(led6,LOW);
-    digitalWrite(led1,LOW);
+    root.printTo(Serial1);
+  }
+    
+
+
+
    
-    digitalWrite(led3,HIGH);
 
-    digitalWrite(led7,LOW);
-    digitalWrite(led8,HIGH);
-  
-    digitalWrite(led9,HIGH);
-    
-    
-  }
+  //tripple condition
+if((inches1>0 && inches1<7)&&(inches2>0 && inches2<7)&&(inches3>0 && inches3<7)&&(inches4>0 && inches4<7)&&(inches5>0 && inches5<7)&&(inches6>0 && inches6<7))
+ {
+  digitalWrite(led1,1); //enables the 1st set of signals
+  digitalWrite(led6,1);
+  digitalWrite(led9,1);
+  digitalWrite(led3,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led4,0);
+  delay(3000);
+ 
+  digitalWrite(led2,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led1,0); 
+  digitalWrite(led6,0);
+  delay(500);
+ 
+  digitalWrite(led3,1); //enables the 2nd set of signals
+  digitalWrite(led4,1);
+  digitalWrite(led9,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led6,0);
+  delay(3000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led9,0);
+  digitalWrite(led4,0);  
+  digitalWrite(led3,0);
+  delay(500); 
+   
+  digitalWrite(led7,1); //enables the 3rd set of signals
+  digitalWrite(led3,1);
+  digitalWrite(led6,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led4,0);
+  digitalWrite(led5,0);
+  digitalWrite(led8,0);
+  digitalWrite(led9,0);
+  delay(3000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led2,1); 
+  digitalWrite(led6,0);
+  digitalWrite(led7,0); 
+  digitalWrite(led3,0);
+  delay(500);   
+}
 
-  else if((inches5>0 && inches5<10)&&(inches6>0 && inches6<10))
+
+   
+   
+   //double conditions
+ 
+ else if((inches1>0 && inches1<7)&&(inches2>0 && inches2<7)&&(inches5>0 && inches5<7)&&(inches6>0 && inches6<7)) //for ultrasonic 1,2 and 5,6
   {
-    stat =1;
-    digitalWrite(led7,HIGH);
-    
-    digitalWrite(led8,LOW);
-    digitalWrite(led9,LOW);
-    digitalWrite(led1,LOW);
-    digitalWrite(led2,HIGH);
+  digitalWrite(led1,1); //enables the 1st set of signals
+  digitalWrite(led6,1);
+  digitalWrite(led9,1);
+  digitalWrite(led3,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led4,0);
+  delay(4000);
+ 
+  digitalWrite(led2,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led1,0); 
+  digitalWrite(led6,0);
+  delay(500);
+  
+  digitalWrite(led7,1); //enables the 3rd set of signals
+  digitalWrite(led3,1);
+  digitalWrite(led6,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led4,0);
+  digitalWrite(led5,0);
+  digitalWrite(led8,0);
+  digitalWrite(led9,0);
+  delay(4000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led2,1); 
+  digitalWrite(led6,0);
+  digitalWrite(led7,0); 
+  digitalWrite(led3,0);
+  delay(500);   
+  
+ 
+  digitalWrite(led3,1); //enables the 2nd set of signals
+  digitalWrite(led4,1);
+  digitalWrite(led9,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led6,0);
+  delay(1000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led9,0);
+  digitalWrite(led4,0);  
+  digitalWrite(led3,0);
+  delay(500); 
+   
+  }   
+else if((inches1>0 && inches1<7)&&(inches2>0 && inches2<7)&&(inches3>0 && inches3<7)&&(inches4>0 && inches4<7))  //for ultrasonic 1,2 and 3,4
+  {
+  digitalWrite(led1,1); //enables the 1st set of signals
+  digitalWrite(led6,1);
+  digitalWrite(led9,1);
+  digitalWrite(led3,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led4,0);
+  delay(4000);
+ 
+  digitalWrite(led2,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led1,0); 
+  digitalWrite(led6,0);
+  delay(500);
+  
+  digitalWrite(led3,1); //enables the 2nd set of signals
+  digitalWrite(led4,1);
+  digitalWrite(led9,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led6,0);
+  delay(4000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led9,0);
+  digitalWrite(led4,0);  
+  digitalWrite(led3,0);
+  delay(500); 
+  
+  digitalWrite(led7,1); //enables the 3rd set of signals
+  digitalWrite(led3,1);
+  digitalWrite(led6,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led4,0);
+  digitalWrite(led5,0);
+  digitalWrite(led8,0);
+  digitalWrite(led9,0);
+  delay(1000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led2,1); 
+  digitalWrite(led6,0);
+  digitalWrite(led7,0); 
+  digitalWrite(led3,0);
+  delay(500);   
+   
+  }   
+else if((inches3>0 && inches3<7)&&(inches4>0 && inches4<7)&&(inches5>0 && inches5<7)&&(inches6>0 && inches6<7)) //for 3,4 and 5,6 ultrasonic
+  {
+  digitalWrite(led3,1); //enables the 2nd set of signals
+  digitalWrite(led4,1);
+  digitalWrite(led9,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led6,0);
+  delay(4000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led9,0);
+  digitalWrite(led4,0);  
+  digitalWrite(led3,0);
+  delay(500); 
+  
+  digitalWrite(led7,1); //enables the 3rd set of signals
+  digitalWrite(led3,1);
+  digitalWrite(led6,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led4,0);
+  digitalWrite(led5,0);
+  digitalWrite(led8,0);
+  digitalWrite(led9,0);
+  delay(4000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led2,1); 
+  digitalWrite(led6,0);
+  digitalWrite(led7,0); 
+  digitalWrite(led3,0);
+  delay(500);  
+  digitalWrite(led1,1); //enables the 1st set of signals
+  digitalWrite(led6,1);
+  digitalWrite(led9,1);
+  digitalWrite(led3,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led4,0);
+  delay(1000);
+ 
+  digitalWrite(led2,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led1,0); 
+  digitalWrite(led6,0);
+  delay(500); 
+   
+  }   
 
-    digitalWrite(led3,HIGH);
-
-    digitalWrite(led4,LOW);
-    digitalWrite(led5,HIGH);
-
-    digitalWrite(led6,HIGH);
-    
-    
+  
+//single conditions
+ else if((inches1>0 && inches1<7)&&(inches2>0 && inches2<7))
+  {
+  digitalWrite(led1,1); //enables the 1st set of signals
+  digitalWrite(led6,1);
+  digitalWrite(led9,1);
+  digitalWrite(led3,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led4,0);
+  delay(3000);
+ 
+  digitalWrite(led2,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led1,0); 
+  digitalWrite(led6,0);
+  delay(500);
+ 
+  digitalWrite(led3,1); //enables the 2nd set of signals
+  digitalWrite(led4,1);
+  digitalWrite(led9,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led6,0);
+  delay(1000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led9,0);
+  digitalWrite(led4,0);  
+  digitalWrite(led3,0);
+  delay(500); 
+   
+  digitalWrite(led7,1); //enables the 3rd set of signals
+  digitalWrite(led3,1);
+  digitalWrite(led6,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led4,0);
+  digitalWrite(led5,0);
+  digitalWrite(led8,0);
+  digitalWrite(led9,0);
+  delay(1000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led2,1); 
+  digitalWrite(led6,0);
+  digitalWrite(led7,0); 
+  digitalWrite(led3,0);
+  delay(500);  
   }
+  
+  else if((inches3>0 && inches3<7)&&(inches4>0 && inches4<7))
+  {
+   digitalWrite(led3,1); //enables the 2nd set of signals
+  digitalWrite(led4,1);
+  digitalWrite(led9,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led6,0);
+  delay(3000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led9,0);
+  digitalWrite(led4,0);  
+  digitalWrite(led3,0);
+  delay(500); 
+  
+  digitalWrite(led7,1); //enables the 3rd set of signals
+  digitalWrite(led3,1);
+  digitalWrite(led6,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led4,0);
+  digitalWrite(led5,0);
+  digitalWrite(led8,0);
+  digitalWrite(led9,0);
+  delay(1000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led2,1); 
+  digitalWrite(led6,0);
+  digitalWrite(led7,0); 
+  digitalWrite(led3,0);
+  delay(500);  
+  
+  digitalWrite(led1,1); //enables the 1st set of signals
+  digitalWrite(led6,1);
+  digitalWrite(led9,1);
+  digitalWrite(led3,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led4,0);
+  delay(1000);
+ 
+  digitalWrite(led2,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led1,0); 
+  digitalWrite(led6,0);
+  delay(500);
+  }
+
+  else if((inches5>0 && inches5<7)&&(inches6>0 && inches6<7))
+  {
+  digitalWrite(led7,1); //enables the 3rd set of signals
+  digitalWrite(led3,1);
+  digitalWrite(led6,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led4,0);
+  digitalWrite(led5,0);
+  digitalWrite(led8,0);
+  digitalWrite(led9,0);
+  delay(3000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led2,1); 
+  digitalWrite(led6,0);
+  digitalWrite(led7,0); 
+  digitalWrite(led3,0);
+  delay(500);  
+  
+  digitalWrite(led1,1); //enables the 1st set of signals
+  digitalWrite(led6,1);
+  digitalWrite(led9,1);
+  digitalWrite(led3,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led4,0);
+  delay(1000);
+ 
+  digitalWrite(led2,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led1,0); 
+  digitalWrite(led6,0);
+  delay(500);
+ 
+  digitalWrite(led3,1); //enables the 2nd set of signals
+  digitalWrite(led4,1);
+  digitalWrite(led9,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led6,0);
+  delay(1000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led9,0);
+  digitalWrite(led4,0);  
+  digitalWrite(led3,0);
+  delay(500);  
+  }
+  
   else
   {
-    digitalWrite(led1,HIGH);
+  digitalWrite(led1,1); //enables the 1st set of signals
+  digitalWrite(led6,1);
+  digitalWrite(led9,1);
+  digitalWrite(led3,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led4,0);
+  delay(2000);
+ 
+  digitalWrite(led2,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led1,0); 
+  digitalWrite(led6,0);
+  delay(500);
+ 
+  digitalWrite(led3,1); //enables the 2nd set of signals
+  digitalWrite(led4,1);
+  digitalWrite(led9,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led5,0);
+  digitalWrite(led7,0);
+  digitalWrite(led8,0);
+  digitalWrite(led6,0);
+  delay(2000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led5,1);
+  digitalWrite(led9,0);
+  digitalWrite(led4,0);  
+  digitalWrite(led3,0);
+  delay(500); 
    
-    digitalWrite(led6,HIGH);
-    
-    
-    digitalWrite(led9,HIGH);
-    delay(200);
+  digitalWrite(led7,1); //enables the 3rd set of signals
+  digitalWrite(led3,1);
+  digitalWrite(led6,1);
+  digitalWrite(led1,0);
+  digitalWrite(led2,0);  
+  digitalWrite(led4,0);
+  digitalWrite(led5,0);
+  digitalWrite(led8,0);
+  digitalWrite(led9,0);
+  delay(2000);
+ 
+  digitalWrite(led8,1); //enables the yellow lights
+  digitalWrite(led2,1); 
+  digitalWrite(led6,0);
+  digitalWrite(led7,0); 
+  digitalWrite(led3,0);
+  delay(500);   
+}
 
-    digitalWrite(led1,LOW);
-   
-    digitalWrite(led6,LOW);
-    
-    
-    digitalWrite(led9,LOW);
-
-    
-    digitalWrite(led4,HIGH);
-    digitalWrite(led3,HIGH);
-    digitalWrite(led9,HIGH);
-    delay(200);
-    digitalWrite(led4,LOW);
-    digitalWrite(led3,LOW);
-    digitalWrite(led9,LOW);
-   
-    digitalWrite(led7,HIGH);
-    digitalWrite(led3,HIGH);
-    digitalWrite(led6,HIGH);
-    delay(200);
-    digitalWrite(led7,LOW);
-    digitalWrite(led3,LOW);
-    digitalWrite(led6,LOW);
-
-  }
-
-  delay(1000);
+  delay(500);
 }
